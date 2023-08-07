fa089a9e1f73369d6c50d1b6a9d1d2b2b1493fab
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-08-07 16:33:29 -0400

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is the Home Page
+      </header>
+    </div>
+  );
+}
+
+export default App;
